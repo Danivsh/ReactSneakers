d16caac35b0ad63c4ff3c8747fdc122a99c77e14
Tasia Danny <tasiadan@pr-k5.kzn.21-school.ru>
2025-01-29 15:36:36 +0300

    Create Components

---
+++ react-sneakers/src/components/Header.js
@@ -0,0 +1,25 @@
+function Headers() {
+  return (
+    <header className="d-flex justify-between align-center p-40">
+      <div className="d-flex align-center">
+        <img width={40} height={40} src="/img/logo.png" alt="Logo" />
+        <div>
+          <h3 className="text-uppercase">React Sneakers</h3>
+          <p className="oppacity-5">Магазин лучших кросовок</p>
+        </div>
+      </div>
+
+      <ul className="d-flex">
+        <li className="mr-30">
+          <img width={18} height={18} src="/img/cart.svg" alt="Cart" />
+          <span>1205 руб</span>
+        </li>
+        <li>
+          <img width={18} height={18} src="/img/user.svg" alt="User" />
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Headers;
